64f0a9c4ff4254a8b55945c885bf3c9ebf12476b
TanuShree7777 <115579296+TanuShree7777@users.noreply.github.com>
2024-02-14 08:54:03 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const account_id = 123456;
+let accountEmail = "asd@yopmail.com";
+var accountPassword = "asd@1234";
+accountCity = "jaipur";
+console.log(accountCity, accountPassword, accountEmail, account_id);
+
+console.table([account_id,accountEmail,accountPassword,accountCity])
