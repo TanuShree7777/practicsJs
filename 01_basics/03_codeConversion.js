e543cfe6c3124f636f541e19e0f4abca67bcf90f
TanuShree7777 <115579296+TanuShree7777@users.noreply.github.com>
2024-02-15 09:24:44 +0000

    conversion,comparison summary

--- 01_basics/03_codeConversion.js
+++ 01_basics/03_codeConversion.js
@@ -11,10 +11,22 @@
 // console.log(typeof valueInNumber, "valueInNumber_Type");
 // let isLoggedIn=1;
 // let isLoggedIn="";
-let isLoggedIn = "tanu";
-let convertBoolean = Boolean(isLoggedIn);
-console.log(convertBoolean);
+// let isLoggedIn = "tanu";
+// let convertBoolean = Boolean(isLoggedIn);
+// console.log(convertBoolean);
 //Notes//
 // 1 => true; 0 => false
 // 11" false
 // "hitesh" true
+
+// link to study https://tc39.es/ecma262/#sec-abstract-operations
+
+console.log(1 + 2 + "3");
+console.log("1+2" + 3);
+console.log("1" + 2 + 3);
+console.log(+true);
+console.log(+"");
+let x=3;
+// x++;
+++x;
+console.log(x);
---
+++ 01_basics/04_Comparisons.js
@@ -0,0 +1,19 @@
+console.log("comparisons");
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);
+// The reason is that an equality check == and comparisons > <
+// <= work differently.
+// Comparisons convert null to a number, treating it as 0.
+// That's why (3) null >= 0 is true and (1)null>0 isfalse.
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+
+//===
+console.log("2" == 2);
+console.log("2" === 2); //strict conparison value and type
